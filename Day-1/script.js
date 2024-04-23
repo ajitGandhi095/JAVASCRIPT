fb064fe88e5a542a251844f58311df644fd92dbd
ajitGandhi095 <ajitkumargandhi95@gmail.com>
2024-04-23 09:31:00 +0530

    Initial update

---
+++ Day-1/script.js
@@ -0,0 +1,36 @@
+//Take 2 numbers add them & print their result in console & alert
+
+// console.log("Hello JAVASCRIPT")
+// var a=20;
+// var a= +(prompt("Enter a value"))
+// confirm("are you sure")
+// a= a+5
+// console.log("The value of a is :",a)
+// alert("The value of a is:"+a)
+ 
+// var a= +prompt("Enter a value")
+// var b= +prompt("Enter a value")
+// c= a + b 
+// console(c) 
+// alert(c)
+
+//data types demo
+
+// var d=25;
+// console.log(typeof(d)) //Number data types is used for numbers including decimals/
+
+// var e="raj"
+// console.log(typeof(e)) //String is used for alphabets, numbers, alphanumeric mixture
+//In JS Strings, there is no difference between '' & ""
+
+// var f= true;
+
+var d=25.3546;
+var e=35.457;
+console.log(typeof(d))
+
+console.log(d.toFixed(2))
+
+console.log(d.toPrecision(2))
+
+console.log(e.toString(2))
